d2a41136e7aa1d9ae06b86665c7595cac4c813ec
Andrew Sumin <sumin.andrew@gmail.com>
2018-10-29 12:58:41 +0300

    t

--- tassandra/request.py
+++ tassandra/request.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from tornado.ioloop import IOLoop
+
 
 class Request(object):
     def __init__(self, query, future, timeouts, tries=0):
@@ -17,13 +19,12 @@ class Request(object):
     def is_retry_possible(self):
         return self.tries < len(self.timeouts)
 
-    def add_timeout(self, io_loop, timeout_callback):
-        self._timeout_handler = io_loop.add_timeout(io_loop.time() + self.timeouts[self.tries],
-                                                    timeout_callback)
+    def add_timeout(self, timeout_callback):
+        self._timeout_handler = IOLoop.current().call_later(self.timeouts[self.tries], timeout_callback)
 
-    def register_response(self, io_loop, response):
+    def register_response(self, response):
         if self._timeout_handler is not None:
-            io_loop.remove_timeout(self._timeout_handler)
+            IOLoop.current().remove_timeout(self._timeout_handler)
             self._timeout_handler = None
         self.tries += 1
         self.failed = isinstance(response, Exception)
